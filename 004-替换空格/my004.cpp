bae0cc89389149307b5bd2b3227c5ba0b4911e65
Alvin Liu <52378184+lssyyly@users.noreply.github.com>
2020-05-02 23:20:31 +0800

    My answer to 004
    My answer to 004

---
+++ 004-替换空格/my004.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+	static void replaceSpace(char *a)
+	{
+		int length = strlen(a);
+		int count = 0;
+		for (int i = 0; i < length; i++)
+		{
+			if (a[i] == ' ')
+			{
+				count++;
+			}
+		}
+		int length2 = length + count * 2;
+		for (int i = length2-1, j = length-1; i >= 0 && j >= 0; j--) 
+		{
+			if (a[j] == ' ')
+			{
+				a[i] = '0';
+				a[i - 1] = '2';
+				a[i - 2] = '%';
+				i -= 3;
+			}
+			else
+			{
+				a[i] = a[j];
+				i--;
+			}
+		}
+		a[length2] = '\0';
+	}
+};
+
+int main()
+{
+    //std::cout << "Hello World!\n"; 
+	char a[100];
+	cin.getline(a, 99);
+	Solution::replaceSpace(a);
+	cout << a << endl;
+}
+
