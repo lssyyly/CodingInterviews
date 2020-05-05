94eea9f3df3d0517d091bc23108af4d9adadbde7
Alvin Liu <52378184+lssyyly@users.noreply.github.com>
2020-05-05 17:51:53 +0800

    My answer to 007
    My answer to 007

---
+++ 007-用两个栈实现队列/my007.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+class solution
+{
+public:
+	int push(int node)
+	{
+		if (stackin.empty()&&!stackout.empty())
+		{
+			while (!stackout.empty())
+			{
+				stackin.push(stackout.top());
+				stackout.pop();
+			}
+			cout << "导入输入栈完成" << endl;
+		}
+		stackin.push(node);
+		return node;
+	}
+
+	int pop()
+	{
+		int node;
+		if (this->isempty())
+		{
+			cout << "error: 队列为空!" << endl;
+			return -1;
+		}
+		if (stackout.empty() && !stackin.empty())
+		{
+			while (!stackin.empty())
+			{
+				stackout.push(stackin.top());
+				stackin.pop();
+			}
+			cout << "导入输出栈完成" << endl;
+		}
+		node = stackout.top();
+		stackout.pop();
+		return node;
+	}
+
+	bool isempty()
+	{
+		return (stackin.empty() && stackout.empty());
+	}
+
+private:
+	stack<int> stackin;
+	stack<int> stackout;
+};
+
+int main()
+{
+	solution solu;
+	for (int i = 0; i < 5; i++)
+	{
+		cout << solu.push(i) << endl;
+	}
+	for (int i = 0; i < 2; i++)
+	{
+		cout << solu.pop() << endl;
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		cout << solu.push(i * 2) << endl;
+	}
+	while (!solu.isempty())
+	{
+		cout << solu.pop() << endl;
+	}
+
+	return 0;
+
+}
+
